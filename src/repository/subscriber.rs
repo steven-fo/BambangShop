c21dd835c5532f2d7f05372f47252d7f1335a6bb
steven-fo <stevenfo0711@gmail.com>
2024-04-11 18:45:55 +0700

    Implement add function in Subscriber repository

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -1,13 +1,21 @@
-use dashmap::Dashmap;
+use dashmap::DashMap;
 use lazy_static::lazy_static;
-use crate::model::subscriber::Subscriber;
+use crate::model::subscriber::{self, Subscriber};
 
 lazy_static! {
-    static ref SUBSRICBERS: Dashmap<String, Dashmap<String, Subscriber>> = Dashmap::new();
+    static ref SUBSRICBERS: DashMap<String, DashMap<String, Subscriber>> = DashMap::new();
 }
 
 pub struct SubscriberRepository;
 
 impl SubscriberRepository {
-    
+    pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
+        let subscriber_value = subscriber.clone();
+        if SUBSRICBERS.get(product_type).is_none() {
+            SUBSRICBERS.insert(String::from(product_type), DashMap::new());
+        };
+
+        SUBSRICBERS.get(product_type).unwrap().insert(subscriber_value.url.clone(), subscriber_value);
+        return subscriber;
+    }
 }
