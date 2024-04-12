4d40fd70dd46d6e80506ee2542b4769f33c1062d
steven-fo <stevenfo0711@gmail.com>
2024-04-12 17:37:21 +0700

    Implement notify function in Notification service to notify each Subscriber

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -2,10 +2,13 @@ use std::thread;
 
 use bambangshop::{Result, compose_error_response};
 use rocket::http::Status;
+use crate::controller::notification::subscribe;
 use crate::model::notification::Notification;
 use crate::model::product::Product;
 use crate::model::subscriber::Subscriber;
 use crate::repository::subscriber::SubscriberRepository;
+
+use super::product;
 
 pub struct NotificationService;
 
@@ -25,5 +28,23 @@ impl NotificationService {
             return Err(compose_error_response(Status::NotFound, String::from("Subscriber not found.")));
         }
         return Ok(result.unwrap());
+    }
+
+    pub fn notify(&self, product_type: &str, status: &str, product: Product) {
+        let mut payload: Notification = Notification {
+            product_title: product.clone().title,
+            product_type: String::from(product_type),
+            product_url: product.clone().get_url(),
+            subscriber_name: String::from(""),
+            status: String::from(status)
+        };
+
+        let subscribers: Vec<Subscriber> = SubscriberRepository::list_all(product_type);
+        for subscriber in subscribers {
+            payload.subscriber_name = subscriber.clone().name;
+            let subscriber_clone = subscriber.clone();
+            let payload_clone = payload.clone();
+            thread::spawn(move || subscriber_clone.update(payload_clone));
+        }
     }
 }
